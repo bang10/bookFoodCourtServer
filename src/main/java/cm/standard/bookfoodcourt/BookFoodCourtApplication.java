d4b21f52de3b4d4e78554d5c34a1fc521cfa0644
bangseonghwan <bang369953@gmail.com>
2024-10-09 10:04:08 +0900

    init commit

---
+++ src/main/java/cm/standard/bookfoodcourt/BookFoodCourtApplication.java
@@ -0,0 +1,13 @@
+package cm.standard.bookfoodcourt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookFoodCourtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookFoodCourtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cm/standard/bookfoodcourt/BookFoodCourtApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.standard.bookfoodcourt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookFoodCourtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
