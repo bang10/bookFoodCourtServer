bd2f9b70b9a5e8dd7bb404c87b148f50c6553ef3
bangseonghwan <bang369953@gmail.com>
2024-10-10 16:27:38 +0900

    번호 인증 확인

--- src/main/java/cm/standard/bookfoodcourt/auth/controller/SmsAuthController.java
+++ src/main/java/cm/standard/bookfoodcourt/auth/controller/SmsAuthController.java
@@ -1,6 +1,7 @@
 package cm.standard.bookfoodcourt.auth.controller;
 
 import cm.standard.bookfoodcourt.auth.service.SmsAuthService;
+import cm.standard.bookfoodcourt.dto.AuthResultDto;
 import cm.standard.bookfoodcourt.util.Common;
 import cm.standard.bookfoodcourt.util.api.ApiResponse;
 import cm.standard.bookfoodcourt.util.redis.RedisService;
@@ -8,10 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.nurigo.sdk.message.response.SingleMessageSentResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
@@ -36,7 +34,7 @@ public class SmsAuthController {
 
         if (smsResult.getStatusCode().equals("2000") || smsResult.getStatusCode().equals("3000") || smsResult.getStatusCode().equals("4000")) {
             // 3분간 저장
-            redisService.saveDateSecond(originTellNumber, randomNumber, 180L);
+            redisService.saveDateSecond(originTellNumber, Integer.toString(randomNumber), 180L);
 
             apiResponse.code = "0";
             apiResponse.message = "성공적으로 전송했습니다. 메시지를 확인해주세요.";
@@ -50,5 +48,49 @@ public class SmsAuthController {
         apiResponse.result = false;
 
         return ResponseEntity.internalServerError().body(apiResponse);
+    }
+
+    @PostMapping("/check/sms")
+    public ResponseEntity<ApiResponse<Boolean>> checkSms(@RequestBody AuthResultDto authResultDto) throws Exception {
+        ApiResponse<Boolean> apiResponse = new ApiResponse<>();
+        if (authResultDto.getSendTo() == null || authResultDto.getCode() == null) {
+            apiResponse.code = "998";
+            apiResponse.message = "전화번호, 인증번호는 필수 값입니다.";
+            apiResponse.result = false;
+
+            return ResponseEntity.ok(apiResponse);
+        }
+        if (authResultDto.getSendTo().isBlank() || authResultDto.getCode().isBlank()) {
+            apiResponse.code = "998";
+            apiResponse.message = "전화번호, 인증번호는 필수 값입니다.";
+            apiResponse.result = false;
+
+            return ResponseEntity.ok(apiResponse);
+        }
+
+        final String checkTellNumberAuth = redisService.getData(authResultDto.getSendTo(), String.class);
+        if (checkTellNumberAuth == null) {
+            apiResponse.code = "999";
+            apiResponse.message = "인증번호 전송이 안되었거나 만료되었습니다. 다시 진행해주세요.";
+            apiResponse.result = false;
+
+            return ResponseEntity.ok(apiResponse);
+        }
+
+        boolean result = checkTellNumberAuth.equals(authResultDto.getCode());
+        if (!result) {
+            apiResponse.code = "998";
+            apiResponse.message = "인증번호가 일치하지 않습니다. 다시 확인해 주세요.";
+            apiResponse.result = false;
+
+            return ResponseEntity.ok(apiResponse);
+        }
+        redisService.deleteData(authResultDto.getSendTo());
+
+        apiResponse.code = "0";
+        apiResponse.message = "인증번호가 일치합니다.";
+        apiResponse.result = true;
+
+        return ResponseEntity.ok(apiResponse);
     }
 }
--- src/test/java/cm/standard/bookfoodcourt/AuthTest.java
+++ src/test/java/cm/standard/bookfoodcourt/AuthTest.java
@@ -1,6 +1,7 @@
 package cm.standard.bookfoodcourt;
 
 import cm.standard.bookfoodcourt.auth.service.SmsAuthService;
+import cm.standard.bookfoodcourt.util.redis.RedisService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,8 +11,16 @@ public class AuthTest {
     @Autowired
     private SmsAuthService smsAuthService;
 
+    @Autowired
+    private RedisService redisService;
+
     @Test
     public void smsTest() throws Exception {
         System.out.println(smsAuthService.sendSmsOne("01050514790", "333321"));
+    }
+
+    @Test
+    public void redisDelete() {
+        redisService.deleteData("01050514790");
     }
 }
